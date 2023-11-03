4b103eaab2c5937686dadfb45e52cb3bc0259ec1
CarrotSea233 <2733614757@qq.com>
2023-11-03 22:09:27 +0800

    AIGC

--- alinesno-infra-base-email-facade/src/main/java/com/alinesno/infra/base/email/entity/UserLabelEntity.java
+++ alinesno-infra-base-email-facade/src/main/java/com/alinesno/infra/base/email/entity/UserLabelEntity.java
@@ -1,5 +1,8 @@
 package com.alinesno.infra.base.email.entity;
 
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -11,23 +14,29 @@ import com.baomidou.mybatisplus.annotation.TableName;
  * @version 1.0.0
  */
 @TableName("user_label")
+@Data
 public class UserLabelEntity extends InfraBaseEntity {
     /**
      * 关系唯一标识符
      */
+	@ColumnType(length=255)
+	@ColumnComment("关系唯一标识符")
+	@TableField("id")
     private Long id;
 
     /**
      * 用户的id
      */
     @TableField("user_id")
+	@ColumnType(length=50)
+	@ColumnComment("用户的id")
     private Long userId;
 
     /**
      * 标签的id
      */
     @TableField("label_id")
+	@ColumnType(length=10)
+	@ColumnComment("标签的id")
     private Long labelId;
-
-    // 省略 getter 和 setter 方法
 }
